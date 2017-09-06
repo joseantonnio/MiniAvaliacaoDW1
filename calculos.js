e68686947587a4e8d9d6e313763522b59ef17ccf
Alex Souza <souzzalex@gmail.com>
2017-09-05 23:57:35 -0300

    Criação da calculadora

---
+++ calculos.js
@@ -0,0 +1,67 @@
+/*
+ * Função Calcular
+ * Recebe como parametro uma string, que identifica qual a operação a ser utilizada no calculo
+ */
+function calculadora(operacao) {
+
+    var valor1 = document.getElementById('numero1').value; // Armazena o valor do 1º número
+    var valor2 = document.getElementById('numero2').value; // Armazena o valor do 2º número
+
+    // Verifica se um dos campos está vazio
+    if ((valor1 == "") || (valor2 == "")) {
+
+        alert("Preencha os 2 valores!!");
+
+    } else {
+
+        //Converte a variavel para inteiro
+        valor1 = parseInt(valor1);
+        valor2 = parseInt(valor2);
+
+        // Verifica qual é a operação, e chama a função enviando como parametros, os 2 numeros para realizar os calculos
+        if (operacao == "adicao") {
+
+            adicao(valor1, valor2);
+
+        } else if (operacao == "subtracao") {
+
+            subtracao(valor1, valor2)
+
+        } else if (operacao == "multiplicacao") {
+
+            multiplicacao(valor1, valor2);
+
+        } else if (operacao == "divisao") {
+
+            divisao(valor1, valor2);
+
+        } else {
+            alert("Operação invalida!");
+        }
+
+    }
+
+}
+
+function adicao(valor1, valor2) {
+
+    var resposta = valor1 + valor2;
+
+    document.getElementById("resultado").value = resposta;
+}
+function multiplicacao(valor1, valor2) {
+
+    var resposta = valor1 * valor2;
+    document.getElementById("resultado").value = resposta;
+}
+function subtracao(valor1, valor2) {
+
+    var resposta = valor1 - valor2;
+    document.getElementById("resultado").value = resposta;
+}
+function divisao(valor1, valor2) {
+
+    var resposta = parseFloat(valor1) / parseFloat(valor2);
+    document.getElementById("resultado").value = resposta.toFixed(3);;
+}
+
